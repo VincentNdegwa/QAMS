5bfba363dec487c6fa9c83720a27b22cab33821c
VincentNdegwa <ndegwavincent7@gmail.com>
2024-06-01 23:12:23 +0300

    made some login interface change

--- resources/js/Pages/ErrorPage.vue
+++ resources/js/Pages/ErrorPage.vue
@@ -1,13 +1,28 @@
 <template>
     <H1>{{ message }}</H1>
+    <div class="d-flex align-items-center justify-content-center">
+        <img
+            src="/images/sad-face.png"
+            class="sad-image align-items-center"
+            alt="sad face"
+        />
+    </div>
 </template>
 <script>
 export default {
     props: {
         message: {
             type: String,
-            default: ""
-        }
-    }
-}
+            default: "",
+        },
+    },
+};
 </script>
+<style>
+.sad-image {
+    width: 100%;
+    height: 100% !important;
+    max-width: 200px !important;
+    max-height: 200px !important;
+}
+</style>
