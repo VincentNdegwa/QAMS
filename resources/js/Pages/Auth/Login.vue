46624b0c19e9066a15b6930a88debe1ca121f622
VincentNdegwa <ndegwavincent7@gmail.com>
2024-06-01 22:46:51 +0300

    made some login interface change

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,38 +1,66 @@
 <template>
-
     <Head title="Login" />
     <div class="container container-section">
         <div class="row d-flex align-items-center">
-            <div class="col image_holder align-items-center d-md-flex d-none ">
-                <img src="images/testing.png" alt="">
+            <div class="col image_holder align-items-center d-none d-md-flex">
+                <img src="images/testing.png" alt="" />
             </div>
             <div class="col d-flex align-items-center text-primary">
                 <div class="form_holder w-100 h-100">
                     <h4 class="card-title text-center mb-5">Login</h4>
-                    <ul v-if="Object.keys(errors).length > 0" class="alert alert-danger">
+                    <ul
+                        v-if="Object.keys(errors).length > 0"
+                        class="alert alert-danger"
+                    >
                         <li v-for="(error, key) in errors" :key="key">
                             {{ error }}
                         </li>
                     </ul>
                     <form @submit.prevent="login">
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email address</label>
-                            <input type="email" class="form-control form-control-lg" id="email" v-model="form.email"
-                                required>
+                            <label for="email" class="form-label"
+                                >Email address</label
+                            >
+                            <input
+                                type="email"
+                                class="form-control form-control-lg"
+                                id="email"
+                                v-model="form.email"
+                                required
+                            />
                         </div>
                         <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control form-control-lg" id="password"
-                                v-model="form.password" required>
+                            <label for="password" class="form-label"
+                                >Password</label
+                            >
+                            <input
+                                type="password"
+                                class="form-control form-control-lg"
+                                id="password"
+                                v-model="form.password"
+                                required
+                            />
                         </div>
-                        <button type="submit" class="btn btn-primary w-100 btn-lg">Login</button>
+                        <button
+                            type="submit"
+                            class="btn btn-primary w-100 btn-lg"
+                        >
+                            Login
+                        </button>
                         <div class="row mt-3">
                             <div class="col-6">
-                                <a href="/forgot-password" class="p text-decoration-none text-secondary">Forgot
-                                    password?</a>
+                                <a
+                                    href="/forgot-password"
+                                    class="p text-decoration-none text-secondary"
+                                    >Forgot password?</a
+                                >
                             </div>
                             <div class="col-6">
-                                <a href="/register" class="p text-decoration-none text-light">Don't have an account?</a>
+                                <a
+                                    href="/register"
+                                    class="p text-decoration-none text-light"
+                                    >Don't have an account?</a
+                                >
                             </div>
                         </div>
                     </form>
@@ -43,32 +71,32 @@
 </template>
 
 <script>
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Head, Link, useForm } from "@inertiajs/vue3";
 export default {
-    props: { "errors": Object },
+    props: { errors: Object },
     data() {
         const form = useForm({
-            email: '',
-            password: '',
+            email: "",
+            password: "",
         });
         return {
-            form
+            form,
         };
     },
     methods: {
         login() {
-            this.form.post(route("login"))
-        }
+            this.form.post(route("login"));
+        },
     },
     components: {
-        Head
-    }
+        Head,
+    },
 };
 </script>
 
 <style>
 .container-section,
-.container-section>div {
+.container-section > div {
     height: 100vh;
 }
 
